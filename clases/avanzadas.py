53f178e0317c196223a38d6cd70d77562e75864e
Karla Rivas Soto <64115leo@universidaddeleon.edu.mx>
2025-10-01 09:19:25 -0600

    Versión 1 - feature/karla

--- clases/avanzadas.py
+++ clases/avanzadas.py
@@ -30,6 +30,9 @@ class Operaciones:
             self.resultado = "El numero " + str(self.num2) + "es mayor que " + str(self.num1)
             print('hola')
 
+    def raiz(self):
+        self.resultado = "La raiz de " + str(self.num1) + " a la " + str(self.num2) + " es igual a " + str(self.num1**(1/self.num2))
+   
     
     def mostrarResultado(self):
         print(self.resultado)
